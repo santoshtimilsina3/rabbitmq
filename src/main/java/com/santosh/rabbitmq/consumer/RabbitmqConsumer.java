df8353994af53b8bb313dac644cbee45affbfab3
Santosh Timilisina <santos.timil@gmail.com>
2023-05-22 22:44:09 +0545

    rabbit-mq-consumer/subscriber

---
+++ src/main/java/com/santosh/rabbitmq/consumer/RabbitmqConsumer.java
@@ -0,0 +1,17 @@
+package com.santosh.rabbitmq.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitmqConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitmqConsumer.class);
+
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
+    public void consume(String message) {
+        LOGGER.info(String.format("Received message -> %s", message));
+        }
+}
